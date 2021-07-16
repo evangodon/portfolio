d21616a678122ae6de60bebbb544e29b91d95e6b
Evan Godon <e.godon@outlook.com>
2021-07-16 18:14:18 -0400

    chore: update config for Linaria

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,20 @@
-import { defineConfig } from "vite";
-import preact from "@preact/preset-vite";
+import { defineConfig } from 'vite';
+import preact from '@preact/preset-vite';
+import linaria from 'vite-plugin-linaria-styled';
 
 export default defineConfig({
-	plugins: [preact()],
+  plugins: [
+    preact(),
+    linaria({
+      sourceMap: true,
+      cacheDirectory: '.linaria-cache',
+      extension: '.linaria.css',
+    }),
+  ],
+  resolve: {
+    alias: {
+      react: 'preact-compat',
+      'react-dom': 'preact-compat',
+    },
+  },
 });
