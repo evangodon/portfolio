e8c0fba3618dd13567b69c9b6f28dd5dbcae3b5e
Evan Godon <e.godon@outlook.com>
2021-08-08 21:12:55 -0400

    chore: update vitejs config for stylis

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 import linaria from 'vite-plugin-linaria-styled';
+import stylis from 'stylis';
+
+stylis.set({ prefix: false });
 
 export default defineConfig({
   plugins: [
@@ -9,6 +12,8 @@ export default defineConfig({
       sourceMap: true,
       cacheDirectory: '.linaria-cache',
       extension: '.linaria.css',
+      displayName: true,
+      preprocessor: 'stylis',
       babelOptions: {
         presets: ['@babel/preset-typescript'],
       },
