44c170c05fbe81d57867d925a597e5e3c8c9a569
Evan Godon <e.godon@outlook.com>
2021-08-11 23:11:37 -0400

    style: glitch text right away

--- src/components/glitch/GlitchEachWord.tsx
+++ src/components/glitch/GlitchEachWord.tsx
@@ -1,16 +1,20 @@
 import { Container } from './Container';
 import { LetterContainer } from './Letter';
+import { useTheme } from '../../context/themeContext';
 
 type Props = {
   text: string;
 };
 
 export const GlitchEachWord = ({ text }: Props) => {
+  const { theme } = useTheme();
   return (
     <>
       {text.split(' ').map((letter, index) => (
         <Container key={index}>
-          <LetterContainer data-text={letter}>{letter}</LetterContainer>
+          <LetterContainer data-text={letter} key={theme}>
+            {letter}
+          </LetterContainer>
         </Container>
       ))}
     </>
